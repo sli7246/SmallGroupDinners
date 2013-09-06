ef559e78770ff5bd9f90f59d5ae9ad651462ae7b
John Li <johnsli@msn.com>
2013-09-06 14:37:22 -0400

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmallGroupDinners::Application.config.secret_key_base = '5089c3965ffdebdfb0144be13c7d2312c8b4068e57cdebfe831e4b298e60dd3337d50a3ba50e2ddf6bf299ea248908ca977c42fc032ac340a73f40d49fac5bd7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SmallGroupDinners::Application.config.session_store :cookie_store, key: '_SmallGroupDinners_session'
